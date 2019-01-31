cf424e9c64d7d134ca255823fbb8fd64ac214ab9
Yacinth <fevriercjjc@gmail.com>
2019-01-31 07:50:33 +0100

    Migration, Model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+p "destroy database"
+User.destroy_all
+Pin.destroy_all
+Comment.destroy_all
+
+p "delete from sqlite_sequence"
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'users'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'pins'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'comments'")
+
+
+10.times do 
+  user = User.create!(name: Faker::Science.scientist)
+  p "user #{user.name}"
+end
+
+10.times do
+  pin = Pin.create(user_id: rand(1..10), url: Faker::Internet.url)
+  p "pin url #{pin.url}"
+end
+
+15.times do
+  comment = Comment.create(user_id: rand(1..10), pin_id: rand(1..10), content: Faker::MichaelScott.quote)
+  p "comment #{comment.content}"
+end
+
+
